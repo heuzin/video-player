8a9020c04bcb7c08dbb57e4719540137f6ff7de3
heuzin <matheusqs.arq@gmail.com>
2020-12-26 20:46:18 -0500

    progress bar

--- script.js
+++ script.js
@@ -32,7 +32,10 @@ video.addEventListener('ended', showPlayIcon)
 
 // Progress Bar ---------------------------------- //
 
-
+// Update Progress bar as video plays
+function updateProgress() {
+    progressbar.style.width = `${(video.currentTime / video.duration) * 100}%`
+}
 
 // Volume Controls --------------------------- //
 
@@ -46,4 +49,6 @@ video.addEventListener('ended', showPlayIcon)
 
 // Event Listners
 playBtn.addEventListener('click', togglePlay)
-video.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress)
